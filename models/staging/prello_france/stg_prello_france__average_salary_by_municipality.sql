d21bbc0dee345ef1e79e25cce0f5c841d585e626
David delmor <david.dlm.lewagon@gmail.com>
2025-06-03 13:55:12 +0000

    updated kpi for average_salary, vacancy_rate and percentage_population_growth

--- models/staging/prello_france/stg_prello_france__average_salary_by_municipality.sql
+++ models/staging/prello_france/stg_prello_france__average_salary_by_municipality.sql
@@ -1,23 +1,21 @@
-with salaries as (
+with 
 
-    select *
-    from {{ ref('stg_prello_france__average_salary_by_municipality') }}
-    where municipality_code is not null
-      and year is not null
+source as (
+
+    select * from {{ source('prello_france', 'average_salary_by_municipality') }}
 
 ),
 
-latest_salary as (
+cleaned as (
 
-    select *
-    from salaries
-    qualify row_number() over (
-        partition by municipality_code
-        order by year desc
-    ) = 1
+    select
+        municipality_code,
+        avg_net_salary,
+        year
+
+    from source
+    where municipality_code is not null 
 
 )
 
-select *
-from latest_salary
-
+select * from cleaned
