dce17226921ce79466c5c38ec4ce2a05cb90a385
Louise GILBERT <louisep.gilbert@gmail.com>
2025-06-03 09:01:09 +0000

    Changed to two decimals

--- models/intermediate/int_prello_france_second_home_ratio.sql
+++ models/intermediate/int_prello_france_second_home_ratio.sql
@@ -4,6 +4,6 @@ SELECT
   nb_tot_housing,
   CASE 
     WHEN nb_tot_housing = 0 THEN NULL
-    ELSE (nb_second_home / nb_tot_housing) * 100
+    ELSE ROUND((nb_second_home / nb_tot_housing) * 100, 2)
   END AS second_home_ratio_pct
 FROM {{ ref('stg_prello_france__housing_stock_clean') }}
