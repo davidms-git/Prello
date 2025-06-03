11102f95209576390dfb720acdc64d3bc6155bf0
David delmor <david.dlm.lewagon@gmail.com>
2025-06-03 08:21:48 +0000

    staging table cleaned real_estate_info_by_municipality

--- models/staging/prello_france/stg_prello_france__real_estate_info_by_municipality.sql
+++ models/staging/prello_france/stg_prello_france__real_estate_info_by_municipality.sql
@@ -9,19 +9,30 @@ source as (
 renamed as (
 
     select
-        municipality_code,
-        intensite_tension_immo,
-        rental_max_apartment,
-        rental_min_apartment,
-        rental_med_house,
-        rental_max_house,
-        rental_min_house,
-        rental_med_all,
-        rental_max_all,
-        rental_min_all
+        cast(municipality_code as string) as municipality_code,
+        cast(intensite_tension_immo as float64) as intensite_tension_immo,
+        cast(rental_max_apartment as float64) as rental_max_apartment,
+        cast(rental_min_apartment as float64) as rental_min_apartment,
+        cast(rental_med_house as float64) as rental_med_house,
+        cast(rental_max_house as float64) as rental_max_house,
+        cast(rental_min_house as float64) as rental_min_house,
+        cast(rental_med_all as float64) as rental_med_all,
+        cast(rental_max_all as float64) as rental_max_all,
+        cast(rental_min_all as float64) as rental_min_all
 
     from source
 
+),
+
+filtered as (
+
+    -- Remove rows with nulls in key fields
+    select *
+    from renamed
+    where municipality_code is not null
+      and intensite_tension_immo is not null
+      and rental_med_all is not null
+
 )
 
-select * from renamed
+select* from filtered
