8e7d00013ceb90f01d3ec9181802ef896d197a53
Louise GILBERT <louisep.gilbert@gmail.com>
2025-06-03 13:20:18 +0000

    Changed KPI name to ratio and not pct

--- models/intermediate/int_prello_france_kpi_second_home_ratio.sql
+++ models/intermediate/int_prello_france_kpi_second_home_ratio.sql
@@ -7,5 +7,5 @@ SELECT
   CASE 
     WHEN nb_tot_housing = 0 THEN NULL
     ELSE ROUND((nb_second_home / nb_tot_housing) , 2)
-  END AS second_home_ratio_pct
+  END AS second_home_ratio
 FROM {{ ref('stg_prello_france__housing_stock_clean') }}
