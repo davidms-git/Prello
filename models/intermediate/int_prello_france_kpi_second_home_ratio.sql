e2edd9ee413d6705f96f4b60501deb2c907334fa
Louise GILBERT <louisep.gilbert@gmail.com>
2025-06-03 13:17:00 +0000

    Changed the percentage to a 0 to 1 ratio

--- models/intermediate/int_prello_france_kpi_second_home_ratio.sql
+++ models/intermediate/int_prello_france_kpi_second_home_ratio.sql
@@ -6,6 +6,6 @@ SELECT
   nb_tot_housing,
   CASE 
     WHEN nb_tot_housing = 0 THEN NULL
-    ELSE ROUND((nb_second_home / nb_tot_housing) * 100, 2)
+    ELSE ROUND((nb_second_home / nb_tot_housing) , 2)
   END AS second_home_ratio_pct
 FROM {{ ref('stg_prello_france__housing_stock_clean') }}
